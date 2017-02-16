3515f51780ab3ebc43c0161fae82ea2dfc20d029
Kentaro Miura <oikimutida@yahoo.co.jp>
2017-02-16 08:32:12 +0000

    basic data type

--- HellorWorld/SampleApp.java
+++ HellorWorld/SampleApp.java
@@ -2,9 +2,29 @@ public class SampleApp {
         public static void main(String[] args) {
             
             // 変数
-            String msg;
-            msg = "Hello World";
+            String msg1;
+            msg1 = "Hello World";
             
-            System.out.println(msg);
+            System.out.println(msg1);
+            
+            /* 色々な型 */
+            // 1文字
+            char a = 'a';
+            
+            // 整数byte short int long
+            int x = 10;
+            long y = 555555555L;
+            
+            // 浮動小数点数 float double
+            double d = 23445.44;
+            float f = 32.33F;
+            
+            // 論理値
+            boolean flag = true; //false
+            
+            //文字列
+            String msg2 = "Hello \nWorld Again\t!!";
+            System.out.println(msg2);
+
         }
 }
