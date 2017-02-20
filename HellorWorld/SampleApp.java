d747675ada76ea74eab87d6a1b93621ad8be04e8
Kentaro Miura <oikimutida@yahoo.co.jp>
2017-02-20 04:42:22 +0000

    basic if syntax

--- HellorWorld/SampleApp.java
+++ HellorWorld/SampleApp.java
@@ -60,5 +60,22 @@ public class SampleApp {
             int i2 = 10;
             double d2 = (double)i2 / 4;
             System.out.println(d2);
+            
+            /* if */
+            int score = 65;
+            if (score > 80) {
+                System.out.println("Great!");
+            }
+            else if (score > 60){
+                System.out.println("Good!");
+            }
+            else
+            {
+                System.out.println("so so...");
+            }
+            
+            //条件演算子
+            String msg = score > 80 ? "Great!" : "so so...!";
+            System.out.println(msg);
         }
 }
